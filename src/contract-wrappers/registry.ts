d43202c90b3c7c91513b26c97af0247ec975793a
rcimoszko <ryan.cimoszko@gmail.com>
2018-08-29 15:22:35 -0700

    add registry function

--- src/contract-wrappers/registry.ts
+++ src/contract-wrappers/registry.ts
@@ -1,7 +1,20 @@
+import { artifacts } from '../artifacts';
 import { ContractWrapper } from './contract-wrapper';
 
 export class Registry extends ContractWrapper {
   constructor(web3: any, networkId: number) {
     super(web3, networkId);
+  }
+
+  public async getAddress(nameKey: string) {
+    const instance = this._getRegistryInstance();
+    return instance.methods.getAddress(nameKey).call();
+  }
+
+  private _getRegistryInstance() {
+    return new this.web3.eth.Contract(
+      artifacts.Registry.abi,
+      artifacts.Registry.networks[this.networkId].address
+    );
   }
 }

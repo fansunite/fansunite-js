ae3bd8a28af43d24ef681e35b0a18cd632859c48
rcimoszko <ryan.cimoszko@gmail.com>
2018-10-08 15:20:56 -0700

    update signature

--- src/utils/signature-utils.ts
+++ src/utils/signature-utils.ts
@@ -5,7 +5,11 @@ export async function signBet(web3: any, bet: Bet, hash: string, sigMode: string
 
   const r = Buffer.from(sig.substring(0, 64), 'hex');
   const s = Buffer.from(sig.substring(64, 128), 'hex');
-  const v = Buffer.from((parseInt(sig.substring(128, 130), 16) + 27).toString(16), 'hex');
+  let vInt = parseInt(sig.substring(128, 130), 16);
+  if (vInt < 27) {
+    vInt += 27;
+  }
+  const v = Buffer.from(vInt.toString(16), 'hex');
   const mode = Buffer.from(sigMode, 'hex');
 
   return '0x' + Buffer.concat([mode, v, r, s]).toString('hex');
@@ -16,7 +20,11 @@ export async function personalSignBet(web3: any, bet: Bet, hash: string, sigMode
 
   const r = Buffer.from(sig.substring(0, 64), 'hex');
   const s = Buffer.from(sig.substring(64, 128), 'hex');
-  const v = Buffer.from((parseInt(sig.substring(128, 130), 16) + 27).toString(16), 'hex');
+  let vInt = parseInt(sig.substring(128, 130), 16);
+  if (vInt < 27) {
+    vInt += 27;
+  }
+  const v = Buffer.from(vInt.toString(16), 'hex');
   const mode = Buffer.from(sigMode, 'hex');
 
   return '0x' + Buffer.concat([mode, v, r, s]).toString('hex');
